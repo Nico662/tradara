f4d3bdb70e3be1527f410943f8c39e92b70911c0
Nicolas Vidal Correcher <nico@MacBook-Air-de-Nicolas.local>
2026-03-24 11:21:04 +0100

    fix double listen

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const app        = express();
 const httpServer = http.createServer(app);
 const io         = new Server(httpServer, { cors: { origin: '*' } });
 const PORT = process.env.PORT || 3001;
-httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.use(cors());
 
 app.get('/candles', async (req, res) => {
