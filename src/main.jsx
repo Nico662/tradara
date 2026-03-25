3bb1129b70b568ce35c44a48acc7dd9928d632ff
Nicolas Vidal Correcher <nico@MacBook-Air-de-Nicolas.local>
2026-03-25 10:00:57 +0100

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { LangProvider } from './LangContext.jsx';
-
+import { inject } from '@vercel/analytics';
+inject();
 createRoot(document.getElementById('root')).render(
  
     <LangProvider>
